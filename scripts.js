3c3c69bb7db4595a42e89cab2817b8c329fb135e
Joaquin <49909167+joaquingz@users.noreply.github.com>
2024-03-06 11:50:14 -0300

    added CreateGrid function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 const container = document.querySelector('#container');
+const containerSize = container.clientHeight;
+
+function createGrid(number){
+    const gridSize = containerSize/number;
+
+    for(let i=0; i <= number-1; i++){
+        for(let j=0; j <=number-1; j++){
+            const div = document.createElement('div');
+            div.classList.add('grid-item');
+            div.style.width = +gridSize+'px';
+            div.style.height = +gridSize+'px';
+            container.appendChild(div);
+        }
+    }
+}
+
+createGrid(16);
+
+const gridItem = document.querySelector('.grid-item');
+
+gridItem.addEventListener('mouseover', (e) =>{
+    e.target.style.background = 'black';
+})
